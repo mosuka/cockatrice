5189fc619224a06bd2a59c8633f75a23e451699a
Minoru Osuka <minoru.osuka@gmail.com>
2018-10-11 14:05:03 +0900

    Update document contents

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -67,7 +67,6 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-# pygments_style = None
 pygments_style = 'sphinx'
 
 
@@ -76,8 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'sphinxdoc'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
